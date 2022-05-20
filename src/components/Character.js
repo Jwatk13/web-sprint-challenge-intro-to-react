95944022095b656aaceabb57e23bba14a0f3e6d5
Joseph Watkins <josephxwatkins@gmail.com>
2022-05-20 00:45:23 -0400

    setup some components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) => {
+    return (
+        <div className='character-data-wrapper'>
+            <h3>{props.card.name}</h3>
+        </div>
+    );
+}
+
+export default Character;
