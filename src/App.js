0f9c8863db756ae1c817144e3059da121d8f5258
Joseph Watkins <josephxwatkins@gmail.com>
2022-05-20 23:19:49 -0400

    getting data semi-correctly...needs more work

--- src/App.js
+++ src/App.js
@@ -7,28 +7,26 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [characterData, setCharacterData] = useState();
+  const [characterData, setCharacterData] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   useEffect(() => {
-    axios.get(`https://swapi.dev/api/people`)
+    axios.get(`https://swapi.dev/api/people/`)
       .then(res => {
-        // console.log(res.data.results);
-        const characterList = res.data.results
-        setCharacterData(characterList);
+        // console.log(res.data)
+        setCharacterData(res.data);
       })
       .catch(err => console.error(err));
   }, [])
 
-
+ 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      { characterData && <Character card={characterData} />}
-      { console.log(characterData) }
+      {characterData && characterData.map((character, index) => <Character  key={index} card={character.name}/>)}
     </div>
 
   );
--- src/components/Character.js
+++ src/components/Character.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Character = (props) => {
     return (
         <div className='character-data-wrapper'>
-            <h3>{props.card.name}</h3>
+            <h3>Name: {props.card.name}</h3>
         </div>
     );
 }
